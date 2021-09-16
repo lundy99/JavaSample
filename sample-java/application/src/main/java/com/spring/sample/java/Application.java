457245fed2dec925620f4642e4f8b9cf626335f3
Shiena Hirashima <shirashima@tohmatsu.co.jp>
2021-09-16 15:36:10 +0000

    add JDBC setting(on try)

--- sample-java/application/src/main/java/com/spring/sample/java/Application.java
+++ sample-java/application/src/main/java/com/spring/sample/java/Application.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-// import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import com.fw.sample.java.utils.LogUtils;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // @ServletComponentScan({"com.sap.cloud.sdk", "com.spring.sample.java", "com.fw.sample.java"})
-@SpringBootApplication(scanBasePackages = {"com.sap.cloud.sdk", "com.spring.sample.java", "com.fw.sample.java"})
+@SpringBootApplication(scanBasePackages = {"com.sap.cloud", "com.spring.sample.java", "com.fw.sample.java"})
 public class Application extends SpringBootServletInitializer
 {
     @Autowired
@@ -22,13 +23,13 @@ public class Application extends SpringBootServletInitializer
 
     public static void main( final String[] args )
     {
-        //スキャンされているbeanを出力する
-        // AnnotationConfigApplicationContext     applicationContext =
-        //   new AnnotationConfigApplicationContext(Application.class);
+        // スキャンされているbeanを出力する
+        AnnotationConfigApplicationContext     applicationContext =
+          new AnnotationConfigApplicationContext(Application.class);
 
-        // for (String beanName : applicationContext.getBeanDefinitionNames()) {
-        //     System.out.println(beanName);
-        // }
+        for (String beanName : applicationContext.getBeanDefinitionNames()) {
+            System.out.println(beanName);
+        }
         SpringApplication.run(Application.class, args);
     }
 }
--- sample-java/application/src/main/java/com/spring/sample/java/controllers/HelloWorldController.java
+++ sample-java/application/src/main/java/com/spring/sample/java/controllers/HelloWorldController.java
@@ -9,16 +9,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.spring.sample.java.models.HelloWorldResponse;
+import javax.sql.DataSource;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import com.sap.cloud.db.JdbcTemplate;
+
 // import com.fw.sample.java.*;
 @RestController
 public class HelloWorldController 
 {
     private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
+    // @Autowired JdbcTemplate jdbc;
+
     @RequestMapping(path="/hello", method = RequestMethod.GET )
     public ResponseEntity<HelloWorldResponse> getHello( @RequestParam( defaultValue = "world" ) final String name )
     {
         logger.info("I am running!");
+
+        // HelloWorldResponse memo = jdbc.queryForObject(
+        //     "select ORDERID from MINORDERS ",
+        //     new Object[] {ORDERID},
+        //     (rs, num) -> new HelloWorldResponse(rs.getString("ORDERID"))
+        // );
+
+        // return ResponseEntity.ok(memo);
         return ResponseEntity.ok(new HelloWorldResponse(name));
     }
 }
